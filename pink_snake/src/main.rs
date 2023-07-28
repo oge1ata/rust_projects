c379ab67890295595e6510ad1d09cfb848526138
oge1ata <yomacorp54@gmail.com>
2023-07-28 07:31:22 -0400

    Just finished the game

--- pink_snake/src/main.rs
+++ pink_snake/src/main.rs
@@ -3,7 +3,35 @@ extern crate piston_window;
 
 mod draw;
 mod snake;
+mod game;
+
+use piston_window::{PistonWindow, WindowSettings, PressEvent, UpdateEvent};
+use piston_window::types::Color;
+
+use crate::game::Game;
+use crate::draw::to_coord_u32;
+
+const BACK_COLOR: Color = [1.0, 0.75, 0.80, 1.0];
 
 fn main() {
-    println!("Hello, world!");
+    let (width, height) = (30, 30);
+
+    let mut window: PistonWindow = WindowSettings::new(
+        "Snake",
+        [to_coord_u32(width), to_coord_u32(height)],).exit_on_esc(true).build().unwrap();
+    
+    let mut game = Game::new(width, height);
+    while let Some(event) = window.next(){
+        if let Some(piston_window::Button::Keyboard(key)) = event.press_args() {
+            game.key_pressed(key);
+        }
+        window.draw_2d(&event, |c, g, _| {
+            piston_window::clear(BACK_COLOR, g);
+            game.draw(&c, g);
+        });
+
+        event.update(|arg|{
+            game.update(arg.dt);
+        });
+    }
 }
